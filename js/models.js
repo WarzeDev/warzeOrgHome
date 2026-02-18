4bc14ed7f52baf3ae5a136bb0d728f2d26d924a1
Warze <contact@warze.org>
2026-02-18 01:26:57 +0100

    Added some extra info to blockbench models

---
+++ js/models.js
@@ -0,0 +1,79 @@
+let modelImagesSetup = false;
+function setupModelImages() {
+	if (modelImagesSetup) return;
+
+	modelImagesSetup = true;
+
+	const models = [
+		'Acrobat Katana',
+		'Amorrya Mask',
+		'Bunny Totem',
+		'Charlies Trident',
+		'Crystal Totem',
+		'Freaky Pumpkin',
+		'Ice Dagger',
+		'Ice Totem',
+		'Mario Top Hat',
+		'Millies Axe',
+		'Mimikyu Winter Hat',
+		'Moth Antennae',
+		'Nether Totem',
+		'Plompy',
+		'Pumpkin Totem',
+		'Red Spear',
+		'Squidward Totem',
+		'Tinee Horns',
+		'Tinee Scythe',
+		'Tinee Stave',
+		'Wingul Hat',
+		'Winter Hood'
+	];
+
+    const modelComments = {
+        'Acrobat Katana': 'Reskin',
+        'Freaky Pumpkin': 'Edit',
+        'Ice Dagger': 'Reskin',
+        'Ice Totem': 'Edit',
+        'Mimikyu Winter Hat': 'Edit',
+        'Red Spear': 'Reskin',
+        'Tinee Stave': 'Edit',
+        'Wingul Hat': 'Edit',
+        'Winter Hood': 'Reskin'
+    };
+
+    const commentColors = {
+        'Reskin': '#66898550',
+        'Edit': '#73a18150'
+    };
+
+	const modelParentContainer = document.getElementById('modelsFlex');
+
+	models.forEach((model) => {
+		const modelContainer = document.createElement('div');
+		modelContainer.classList.add('modelContainer');
+
+		const img = document.createElement('img');
+		const modelPath = model.toLowerCase().replaceAll(' ', '');
+		img.src = `/models/webp/${modelPath}.webp`;
+		img.alt = model;
+		img.setAttribute('data-full', `/models/apng/${modelPath}.png`);
+		modelContainer.appendChild(img);
+
+        const comment = modelComments[model];
+        if (comment) {
+            const commentElement = document.createElement('p');
+            commentElement.innerText = '*' + comment;
+            commentElement.style.backgroundColor = commentColors[comment];
+            commentElement.classList.add('comment');
+            modelContainer.appendChild(commentElement);
+        }
+
+		const p = document.createElement('p');
+		p.innerText = model;
+		modelContainer.appendChild(p);
+
+		modelParentContainer.appendChild(modelContainer);
+	});
+
+	updateClickableImages();
+}
