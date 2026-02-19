966d18b8dbbea2524d230756aeedd918fc99254f
Warze <contact@warze.org>
2026-02-19 03:03:50 +0100

    Added gob, responsiveness and background optimizations

--- js/stars.js
+++ js/stars.js
@@ -1,31 +1,33 @@
-function clearCanvases() {
-	document.querySelectorAll('.stars').forEach((canvas) => canvas.remove());
-}
-
 function initializeStars() {
-	clearCanvases();
-
 	const starCount = Math.floor((window.innerWidth * window.innerHeight) / 2000);
 	const layers = 5;
 
 	for (let ci = 0; ci < layers; ci ++) {
 		const depth = ci / (layers - 1);
 
-		const cvs = document.createElement('canvas');
-		cvs.classList.add('stars');
+		let cvs;
+		if (document.querySelector(`#stars${ci}`)) {
+			cvs = document.querySelector(`#stars${ci}`);
+		} else {
+			cvs = document.createElement('canvas');
+			document.body.appendChild(cvs);
+			cvs.setAttribute('data-stars-id', ci);
+			cvs.classList.add('stars');
+			cvs.id = `stars${ci}`;
+		}
 		cvs.width = window.innerWidth + 150;
 		cvs.height = window.innerHeight + 150;
 		const ctx = cvs.getContext('2d');
+		ctx.globalAlpha = 1;
+		ctx.clearRect(0, 0, cvs.width, cvs.height);
 		ctx.globalAlpha = 0.1 + depth * 0.1;
 		ctx.fillStyle = `white`;
-		cvs.setAttribute('data-stars-id', ci);
 		const size = 0.5 + ci / layers;
 		for (let i = 0; i < starCount; i++) {
 			ctx.beginPath();
 			ctx.arc(Math.random() * cvs.width, Math.random() * cvs.height, size, 0, Math.PI * 2);
 			ctx.fill();
 		}
-		document.body.appendChild(cvs);
 	}
 }
 
